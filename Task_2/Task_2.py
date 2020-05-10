167c7374e9f98bda2931d89f6b6c69a3dea69f8c
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-10 16:58:00 +0300

    Added all functions for Task 2

--- Task_2/Task_2.py
+++ Task_2/Task_2.py
@@ -5,27 +5,65 @@ mongoClient = MongoClient()
 database = mongoClient.database['python_3k_task2']
 restaurants = database['restaurants']
 
+#https://pymongo.readthedocs.io/en/stable/
+#https://pymongo.readthedocs.io/en/stable/examples/index.html
+
 def first():
-    query = restaurants.find();
-    for record in query:
+    result = restaurants.find();
+    print('Number of records returned: ', restaurants.count_documents({}))
+    for record in result:
         print(record)
 
+def second():
+    result = restaurants.find({}, {'restaurant_id' : 1, 'name': 1, 'borough': 1, 'cuisine': 1})
+    print('Number of records returned: ', restaurants.count_documents({}))
+    for record in result:
+        print(record)
+
+def third():
+    result = restaurants.find({},{"_id": 0, 'restaurant_id' : 1, 'name' : 1, 'borough': 1, 'cuisine': 1})
+    print('Number of records returned: ', restaurants.count_documents({}))
+    for record in result:
+        print(record)
+
+def fourth():
+    query = {'borough': "Bronx"}
+    result = restaurants.find(query)
+    print('Number of records returned: ', restaurants.count_documents(query))
+    for record in result:
+        print(record)
+
+def fifth():
+    query = {'grades': {'$elemMatch': {'score': {'$gte': 80, '$lte': 100 }}}}
+    result = restaurants.find(query)
+    print('Number of records returned: ', restaurants.count_documents(query))
+    for record in result:
+        print(record)
 
 #Examples
 if __name__ == '__main__':
 
     #Seed data from json file
-    #Run on first startup
-    #with open('./Task_2/restaurants.json') as f:
-    #    file_data = json.load(f)
-
-    #restaurants.insert_many(file_data)
+    #Run to seed database, if it's empty
+    if restaurants.count_documents({}) == 0:
+        with open('./Task_2/restaurants.json') as f:
+            file_data = json.load(f)
+        restaurants.insert_many(file_data)
 
     print("First task: \n")
     first()
 
+    print("Second task: \n")
+    second()
 
+    print("Third task: \n")
+    third()
 
+    print("Fourth task: \n")
+    fourth()
 
+    print("Fifth task: \n")
+    fifth()
 
+    #Close client
     mongoClient.close()
