097c95d8bb6e3b592cf61f9dbf0c21d1eaa97734
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-30 16:42:29 +0300

    added user profile edit

--- Task_5/webapp/users/views.py
+++ Task_5/webapp/users/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import UserForm
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Your account was successfully created. You can log in now.')
             return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    user = User.objects.get(pk=request.user.id)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account was successfully saved.')
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'users/profile.html', {'form': form})
