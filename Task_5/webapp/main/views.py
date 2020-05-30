fb474b4f7fb283401edd48b5f1eff4ee7621c3fc
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-30 19:09:11 +0300

    fixed line creation view and model

--- Task_5/webapp/main/views.py
+++ Task_5/webapp/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DetailView
-from .models import Form
+from .models import Form, FormLine
 
 # Create your views here.
 
