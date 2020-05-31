d18557ed7a1b25cf7d5f61ae7c25ab7199e4a7ec
Jonas Petraška <jonas.petraska@gmail.com>
2020-05-31 03:34:55 +0300

    added pdf template and rendering to pdf

--- Task_5/webapp/main/views.py
+++ Task_5/webapp/main/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from .models import Form, FormLine
+from django.template.loader import get_template
+from io import BytesIO
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -10,6 +13,10 @@ from .models import Form, FormLine
 def home(request):
     forms = Form.objects.filter(main_member=request.user)
     return render(request, 'main/home.html', {'forms': forms});
+
+def form_as_pdf(request, *args, **kwargs):
+    template = get_template('pdf.html')
+    data = get_form_pdf_data(kwargs)
 
 class CreateForm(LoginRequiredMixin, CreateView):
     model = Form
@@ -69,3 +76,27 @@ class CreateFormLine(LoginRequiredMixin, CreateView):
         form.instance.form = Form.objects.get(pk=self.kwargs['pk'])
         form.instance.cost = form.instance.sum/form.instance.quantity
         return super().form_valid(form)
+
+def get_form_pdf_data(kwargs):
+    data = Form.objects.get(pk=kwargs['pk'])
+    data_tosend = vars(data)
+    data_tosend['lines'] = data.formline_set.all()
+    data_tosend['main_member'] = data.main_member.username if not data.main_member.first_name else data.main_member.first_name + ' ' + data.main_member.last_name
+    data_tosend['member1'] = data.member1.username if not data.member1.first_name else data.member1.first_name + ' ' + data.member1.last_name
+    data_tosend['member2'] = data.member2.username if not data.member2.first_name else data.member2.first_name + ' ' + data.member2.last_name
+    data_tosend['member3'] = data.member3.username if not data.member3.first_name else data.member3.first_name + ' ' + data.member3.last_name
+    data_tosend['member4'] = data.member4.username if not data.member4.first_name else data.member4.first_name + ' ' + data.member4.last_name
+    total = 0
+    for line in data.formline_set.all():
+        total = total + line.sum
+    data_tosend['line_total'] = total
+    return data_tosend
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
